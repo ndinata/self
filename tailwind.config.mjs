d24f90fcb87a60907240584a47386a159db0f0b1
nico <nicodnt@icloud.com>
2024-12-27 17:24:13 +1100

    use "Jetbrains Mono" as default monospace font (#4)
    use jetbrains-mono as default monospace font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,14 @@
+import defaultTheme from "tailwindcss/defaultTheme";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx}"],
   darkMode: ["class"],
   theme: {
     extend: {
+      fontFamily: {
+        mono: ["JetBrains Mono", ...defaultTheme.fontFamily.mono],
+      },
       container: {
         center: true,
         padding: "2rem",
